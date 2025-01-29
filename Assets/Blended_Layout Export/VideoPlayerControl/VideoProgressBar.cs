8f40bc622b053054d24c843d942ac47697498c16
Jaikishore <jaikishore@dlearners.in>
2025-01-29 18:36:20 +0530

    imported updated video loader

--- Assets/Blended_Layout Export/VideoPlayerControl/VideoProgressBar.cs
+++ Assets/Blended_Layout Export/VideoPlayerControl/VideoProgressBar.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -7,18 +8,89 @@ public class VideoProgressBar : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
     [SerializeField]
     private VideoPlayer videoPlayer;
+    [SerializeField] private GameObject videoLoading, 
+                                        videoLoadingErrorPanel,
+                                        spawnedLoader;
+    public Texture2D thumbnailTexture;
     public Camera Cam;
     public Image progress;
-    
+    double lastTimePlayed;
+    float waitTime = 3f,
+            time = 0f;
+    float skipFrame;
+    bool requestSkip,
+        requestPrevSkip,
+        requestNextSkip,
+        reachedEnd = false,
+        errorOccured = false;
+
     private void Awake()
     {
+        videoPlayer.playOnAwake = false;
+        videoPlayer.isLooping = false;
         Cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        videoPlayer.targetTexture.Release();
+        videoPlayer.loopPointReached += VideoFinished;
+        videoPlayer.errorReceived += VideoPlayerErrorOccured;
+    }
+
+    void VideoFinished(VideoPlayer vp){
+        reachedEnd = true;
+        BlendedOperations.instance.VideoCompleted();
+        DestroyVideoLoader();
+        Main_Blended.OBJ_main_blended.B_pause = true;
+    }
+
+    private void OnEnable()
+    {
+        if (thumbnailTexture != null)
+            Graphics.Blit(thumbnailTexture, videoPlayer.targetTexture);
+        // StartCoroutine(GetThumbnail());
+    }
+
+    IEnumerator GetThumbnail()
+    {
+        videoPlayer.Prepare();
+        while (!videoPlayer.isPrepared)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        videoPlayer.frame = 10;
+        videoPlayer.Play();
+        videoPlayer.Pause();
     }
 
     private void Update()
     {
+        Check_Video_Buffering();
         if (videoPlayer.frameCount > 0)
             progress.fillAmount = (float)videoPlayer.frame / (float)videoPlayer.frameCount;
+    }
+
+    private void Check_Video_Buffering()
+    {
+        if (videoPlayer.isPlaying && (Time.frameCount % (int)(videoPlayer.frameRate + 1)) == 0)
+        {
+            if (lastTimePlayed == videoPlayer.time)
+            {
+                // Debug.Log($"buffering");
+                time += Time.deltaTime;
+                if (time > waitTime){
+                    SpawnVideoLoader();
+                }
+            } else {
+                // Debug.Log($"not buffering");
+                time = 0f;
+                if (spawnedLoader != null)
+                    DestroyVideoLoader();
+            }
+            lastTimePlayed = videoPlayer.time;
+        }
+        else if (!videoPlayer.isPlaying && !reachedEnd && !errorOccured)
+        {
+            SpawnVideoLoader();
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -33,7 +105,13 @@ public class VideoProgressBar : MonoBehaviour, IDragHandler, IPointerDownHandler
 
     private void TrySkip(PointerEventData eventData)
     {
+        if(errorOccured) return;
+
         Vector2 localPoint;
+        if(reachedEnd){
+            videoPlayer.Play();
+            reachedEnd = false;
+        }
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
             progress.rectTransform, eventData.position, Cam, out localPoint))
         {
@@ -44,7 +122,31 @@ public class VideoProgressBar : MonoBehaviour, IDragHandler, IPointerDownHandler
 
     private void SkipToPercent(float pct)
     {
-        var frame = videoPlayer.frameCount * pct;
-        videoPlayer.frame = (long)frame;
+        skipFrame = videoPlayer.frameCount * pct;
+        videoPlayer.frame = (long)skipFrame;
+        SpawnVideoLoader();
+    }
+
+    void SpawnVideoLoader(){
+        if (spawnedLoader == null && !Main_Blended.OBJ_main_blended.B_pause){
+            spawnedLoader = Instantiate(videoLoading, gameObject.transform);
+        }
+    }
+
+    void SpawnVideoLoaderError(){
+        Instantiate(videoLoading, gameObject.transform);
+    }
+
+    void DestroyVideoLoader(){
+        if(spawnedLoader != null)
+            Destroy(spawnedLoader);
+    }
+
+    void VideoPlayerErrorOccured(VideoPlayer vp, string errorMsg){
+        Debug.Log($"{errorMsg}");
+        if(errorOccured) return;
+        errorOccured = true;
+        DestroyVideoLoader();
+        Instantiate(videoLoadingErrorPanel, gameObject.transform);
     }
 }
