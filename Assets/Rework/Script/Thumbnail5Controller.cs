24bd67324a8ed748abff89d9be4b99b96018330d
Jaikishore <jaikishore@dlearners.in>
2025-02-04 18:35:47 +0530

    5th slide asset imported

---
+++ Assets/Rework/Script/Thumbnail5Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thumbnail5Controller : MonoBehaviour
+{
+    [SerializeField] List<CattleData> maleCattleData;
+    [SerializeField] List<CattleData> femaleCattleData;
+    [SerializeField] List<GameObject> forestEnvironmentObjs;
+    [SerializeField] Transform animalDisplayPosition;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+#region ANIMATION
+
+    void ShowFarm(Transform animalObj)
+    {
+        // Utilities.Instance.ANIM_Move(sourceObj, endPosition.position);
+    }
+
+#endregion
+
+    [Serializable]
+    class CattleData
+    {
+        public Sprite cattleSprite;
+        public string cattleType;
+    }
+}
+
