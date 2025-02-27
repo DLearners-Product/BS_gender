eeef6010029042c1827051f66b48faa3b5395149
Jaikishore <jaikishore@dlearners.in>
2025-02-27 18:31:24 +0530

    imported drag and drop script included

---
+++ Assets/Rework/Script/Standalone/DragDrop/ImageDragandDrop.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class ImageDragandDrop : MonoBehaviour, IBeginDragHandler,IEndDragHandler,IDragHandler
+{
+    Vector2 mousePos;
+    Camera mainCam;
+    public CanvasGroup canvasGroup;
+    public Vector3 currentPos;
+    public Vector2 min, max;
+    public Transform originalParent = null;
+    public bool resetPositionOnDrop;
+
+    public delegate void OnDragStartDelegate(GameObject dragObject);
+    public delegate void OnDragDelegate(GameObject dragObject);
+    public delegate void OnDragEndDelegate(GameObject dragObject);
+
+    public static OnDragStartDelegate onDragStart;
+    public static OnDragDelegate onDrag;
+    public static OnDragEndDelegate onDragEnd;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+        canvasGroup = GetComponent<CanvasGroup>();
+        originalParent = this.transform.parent;
+        min = GetComponent<RectTransform>().offsetMin;
+        max = GetComponent<RectTransform>().offsetMax;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        onDragStart.Invoke(eventData.pointerDrag);
+
+        // this.transform.SetParent(originalParent.parent);
+        canvasGroup.alpha = .5f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        onDrag.Invoke(eventData.pointerDrag);
+
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = mousePos;
+        currentPos = mousePos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log($"OnEndDragCalled");
+        onDragEnd.Invoke(eventData.pointerDrag);
+
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        Debug.Log(gameObject.transform.parent.name, gameObject.transform.parent);
+        if(originalParent.Equals(gameObject.transform.parent) && resetPositionOnDrop){
+            ReturnToOriginalPos();
+        }
+    }
+
+    public void ResetParentOriginalPosition()
+    {
+        originalParent = this.transform.parent;
+        min = GetComponent<RectTransform>().offsetMin;
+        max = GetComponent<RectTransform>().offsetMax;
+    }
+
+    public void ReturnToOriginalPos()
+    {
+        transform.SetParent(originalParent);
+        GetComponent<RectTransform>().offsetMin = min;
+        GetComponent<RectTransform>().offsetMax = max;
+    }
+}
---
+++ Assets/Rework/Script/Standalone/DragDrop/ImageDropSlot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+public class ImageDropSlot: MonoBehaviour, IDropHandler
+{
+    ImageDragandDrop dragitem;
+    public delegate void OnDropInSlotDelegate(GameObject dragedObject);
+    public static OnDropInSlotDelegate onDropInSlot;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDropCalled...."+eventData.pointerDrag);
+        dragitem = eventData.pointerDrag.GetComponent<ImageDragandDrop>();
+        onDropInSlot.Invoke(eventData.pointerDrag);
+    }
+
+    public void SetDropedObject(){
+        dragitem.transform.SetParent(this.transform);
+        dragitem.canvasGroup.alpha = 1f;
+        dragitem.transform.localPosition = dragitem.currentPos;
+        dragitem.enabled = false;
+    }
+
+    public void ResetDropedObjectPosition(){
+        dragitem.ReturnToOriginalPos();
+    }
+}
+
